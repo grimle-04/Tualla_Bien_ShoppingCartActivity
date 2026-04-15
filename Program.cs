78492acde04af236612fc7c6300c8f84656c8ea0
grimle-04 <bientualla04@gmail.com>
2026-04-16 07:38:00 +0800

    Create Product class with properties
    Added Product class with properties and constructor.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
+# product class
 
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    public class Product
+
+    {
+        public int Id;
+        public string Name;
+        public double Price;
+        public int RemainingStock;
+
+
+        public Product(int id, string name, double price, int remainingStock)
+
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Price = price;
+            this.RemainingStock = remainingStock;
+        }
